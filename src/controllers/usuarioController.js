3d6eaf4462867d35b23eaecbb8c9774694b45d1f
JoaoVLira <joao.alves@sptech.school>
2025-05-15 22:56:56 -0300

    Funcionalidade de cadastro e mudanças no formulário

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -42,6 +42,7 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var jogador = req.body.jogadorServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -50,10 +51,12 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else {
+    } else if (jogador == undefined) {
+        res.status(400).send("Seu jogador Favorito está undefined!");
+    }else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, email, senha, jogador)
             .then(
                 function (resultado) {
                     res.json(resultado);
