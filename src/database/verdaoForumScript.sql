817a4abbd245453215a3efbe047c9a3a280307c9
JoaoVLira <joao.alves@sptech.school>
2025-05-19 22:23:55 -0300

    Script do Banco de Dados

---
+++ src/database/verdaoForumScript.sql
@@ -0,0 +1,20 @@
+create database verdaoForum;
+
+use verdaoForum;
+
+create table usuario (
+    id int auto_increment primary key,
+    nome varchar(50),
+    email varchar(50),
+    senha varchar(50),
+    jogador_favorito varchar(50)
+);
+
+CREATE TABLE respostas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    fk_usuario INT,
+    jogador_favorito VARCHAR(45),
+    camiseta_favorita VARCHAR(45),
+    formacao_favorita VARCHAR(30),
+    FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
